30c3796ae4830eed4ae7788f7b780b893568ab0d
Shaheer Ahmad <shaheer.ahmad@square63.com>
2023-01-26 23:11:16 +0500

    Built the connection between the view, conttroller and the call to fetch the data.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,36 @@
 class HomeController < ApplicationController
+  before_action :permit_params, only: [:generate_plan]
 
   def index; end
 
   def generate_plan
-    redirect_to show_path
+    locations = params.dig('locations').to_a
+    activities = params[:activities]
+    pace = params[:pace]
+
+    steps, prompt = FetchDataService.new.get_plan(locations, activities, pace)
+
+    puts "\n\n\n#{steps}"
+
+    redirect_to show_path(steps: steps, prompt: prompt)
   end
 
   def show
+    @steps = params[:steps]
+    @prompt = params[:prompt]
   end
+
+  def explain_point
+    @response = FetchDataService.new.explain_point(params[:prompt], params[:point])
+    respond_to do |format|
+      format.turbo_stream {  }
+    end
+  end
+
+  private
+
+  def permit_params
+    params.permit(:pace, activities: [], locations: [:destination, :days])
+  end
+
 end
